285f534fcd251aee5d03accb4c46efa612a670b2
zosma <areaweb80@gmail.com>
2021-12-15 20:41:18 +0100

    Updated the eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,90 +7,91 @@ module.exports = {
     parser: 'babel-eslint',
   },
   extends: [
-    "standard",
-    "react-app",
-    "react-app/jest",
-    "plugin:react/jsx-runtime"
+    'standard',
+    'react-app',
+    'react-app/jest',
+    'plugin:react/jsx-runtime',
+    'plugin:cypress/recommended',
   ],
   rules: {
-    "indent": [
-      "error",
-      2
-    ],
-    "max-len": [
-      "error",
-      {
-        "code": 120
-      }
-    ],
-    "semi": [
+    indent: [
+      'error',
       2,
-      "always"
     ],
-    "comma-dangle": [
-      "error",
-      "always-multiline"
-    ],
-    "space-before-function-paren": [
-      "error",
-      "never"
-    ],
-    "function-paren-newline": [
-      "error",
-      "multiline"
-    ],
-    "no-multiple-empty-lines": [
-      "error",
+    'max-len': [
+      'error',
       {
-        "max": 1,
-        "maxBOF": 0,
-        "maxEOF": 0
-      }
+        code: 120,
+      },
     ],
-    "react/self-closing-comp": [
-      "error",
+    semi: [
+      2,
+      'always',
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'space-before-function-paren': [
+      'error',
+      'never',
+    ],
+    'function-paren-newline': [
+      'error',
+      'multiline',
+    ],
+    'no-multiple-empty-lines': [
+      'error',
       {
-        "component": true,
-        "html": true
-      }
+        max: 1,
+        maxBOF: 0,
+        maxEOF: 0,
+      },
     ],
-    "react/jsx-first-prop-new-line": [
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'react/jsx-first-prop-new-line': [
       1,
-      "multiline"
+      'multiline',
     ],
-    "react/jsx-max-props-per-line": [
+    'react/jsx-max-props-per-line': [
       1,
       {
-        "maximum": 1,
-        "when": "always"
-      }
+        maximum: 1,
+        when: 'always',
+      },
     ],
-    "react/jsx-indent": [
+    'react/jsx-indent': [
       2,
-      2
-    ],
-    "react/jsx-indent-props": [
       2,
-      2
     ],
-    "react/jsx-closing-bracket-location": 1,
-    "react/jsx-pascal-case": "error",
-    "react/jsx-curly-spacing": [
-      "error",
-      "never"
+    'react/jsx-indent-props': [
+      2,
+      2,
     ],
-    "react/jsx-tag-spacing": "error",
-    "react/jsx-wrap-multilines": [
-      "error",
+    'react/jsx-closing-bracket-location': 1,
+    'react/jsx-pascal-case': 'error',
+    'react/jsx-curly-spacing': [
+      'error',
+      'never',
+    ],
+    'react/jsx-tag-spacing': 'error',
+    'react/jsx-wrap-multilines': [
+      'error',
       {
-        "declaration": "parens-new-line",
-        "assignment": "parens-new-line",
-        "return": "parens-new-line",
-        "arrow": "parens-new-line",
-        "condition": "parens-new-line",
-        "logical": "parens-new-line",
-        "prop": "ignore"
-      }
-    ]
-  }
+        declaration: 'parens-new-line',
+        assignment: 'parens-new-line',
+        return: 'parens-new-line',
+        arrow: 'parens-new-line',
+        condition: 'parens-new-line',
+        logical: 'parens-new-line',
+        prop: 'ignore',
+      },
+    ],
+  },
 };
